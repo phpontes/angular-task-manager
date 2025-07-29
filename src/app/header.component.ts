ff435b55d0af2808f0efdc4af58fbd745fb3e174
phpontes <ppontes95@hotmail.com>
2025-07-29 09:19:58 -0300

    configuring the custom component

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component()
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+})
 export class HeaderComponent {}
