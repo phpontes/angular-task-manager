ef29025576c946594b838b4b476c2d30b8941992
phpontes <ppontes95@hotmail.com>
2025-07-30 13:12:42 -0300

    remove unused imports in UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
