c7a6309ec9feaff79a8b32fa7e32633fc27600b8
phpontes <ppontes95@hotmail.com>
2025-07-31 15:06:07 -0300

    format due date display and add DatePipe to task component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 import { type Task } from './task.model';
-import { CardComponent } from "../../@shared/card/card.component";
+import { CardComponent } from '../../@shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
+  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
