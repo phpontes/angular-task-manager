ea67fecc7a4aae67fb26ee3e32be90ca33291c35
phpontes <ppontes95@hotmail.com>
2025-07-31 10:35:02 -0300

    outputting user-specific tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
@@ -9,5 +9,36 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Get a job in IT',
+      summary:
+        'Get a job in IT as a backend developer, so you can pay your debts and won\'t have to work as a "peão de fábrica" never again.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Finish Angular course',
+      summary:
+        'Finish this Angular course before your will to live expires. Bonus points if you actually understand RxJS.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Deploy something (anything)',
+      summary:
+        "Deploy a project to impress recruiters — even if it's just a ToDo app that barely works, hosted on a free tier cloud provider held together by duct tape.",
+      dueDate: '2025-12-31',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
