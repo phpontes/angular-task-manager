42830f57556e425cd59dac75367cd0c3aa3cf12f
phpontes <ppontes95@hotmail.com>
2025-08-06 06:32:02 -0300

    feat: add TasksModule and integrate task related components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './@shared/shared.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { TasksModule } from './tasks/tasks.module';
 import { UserComponent } from './user/user.component';
-import { SharedModule } from './@shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
