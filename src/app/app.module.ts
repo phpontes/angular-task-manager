0026b17452d7898036368e50d68f17223fdef801
phpontes <ppontes95@hotmail.com>
2025-08-06 06:21:44 -0300

    feat: add SharedModule and integrate CardComponent; update title in index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { CardComponent } from './@shared/card/card.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './@shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
